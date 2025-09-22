f9490fc7b054af2bb8c2405c87d1e7426d0aa422
margihingrajia <margi.hingrajia@gmail.com>
2025-09-22 14:19:36 -0400

    publish web

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,22 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
+
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+
+    kit: {
+    adapter: adapter(),
+    paths: {
+      // 👇 replace USERNAME and REPO with your GitHub username & repo name
+      base: dev ? '' : '/Project-1',
+    }
+  }
 }
+
+
